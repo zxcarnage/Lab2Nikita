c39f0501c6cc5a6568b0420b5ebe883ff58b991b
zxcarnage <nmoysenovich@gmail.com>
2023-12-03 11:50:05 +0300

    finished

---
+++ Assets/Scripts/Computer/Computer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Computer : MonoBehaviour
+{
+    [SerializeField] private BallMover _ball;
+    [SerializeField] private float _computerSpeed;
+
+    private Rigidbody2D _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+    
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var movingVector =  new Vector3(transform.position.x, _ball.transform.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, movingVector, _computerSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/LoseZone.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LoseZone : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.TryGetComponent(out BallMover ball))
+            ResetScene();
+    }
+    
+    private void ResetScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
